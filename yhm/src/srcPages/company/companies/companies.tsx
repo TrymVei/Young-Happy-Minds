17a28201d53a26ced8bc216a30b77c10c37634be
TrymVei <tve@variant.no>
2022-09-28 10:20:30 +0200

    feat: company animation first draft

--- yhm/src/srcPages/company/companies/companies.tsx
+++ yhm/src/srcPages/company/companies/companies.tsx
@@ -1,12 +1,35 @@
+import anime from 'animejs';
+import { useEffect } from 'react';
 import style from './companies.module.css';
 const Companies = () => {
+  const animate = () => {
+    anime({
+      targets: '#carousell img',
+      translateX: 2000,
+      loop: true,
+      duration: 15000,
+      easing: 'linear',
+    });
+  };
   return (
     <div id="section" className={style.companies}>
       <p>Noen av firmaene vi har kurset:</p>
-      <div className={style.companies__wrapper}>
+      <div
+        onClick={animate}
+        id="carousell"
+        className={style.companies__wrapper}
+      >
         <img src="/images/companies/companies-1.svg" alt="" />
         <img src="/images/companies/companies-2.svg" alt="" />
         <img src="/images/companies/companies-3.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
+        <img src="/images/companies/companies-4.svg" alt="" />
         <img src="/images/companies/companies-4.svg" alt="" />
       </div>
     </div>
