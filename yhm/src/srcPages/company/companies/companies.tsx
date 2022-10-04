5f89f3aba603dc860e0b5c401ac84394bf9c9c99
TrymVei <tve@variant.no>
2022-10-04 10:37:42 +0200

    fix: better sliding carousell

--- yhm/src/srcPages/company/companies/companies.tsx
+++ yhm/src/srcPages/company/companies/companies.tsx
@@ -3,48 +3,35 @@ import { useEffect, useState } from 'react';
 import style from './companies.module.css';
 
 const Companies = () => {
-  const animate = () => {
-    anime({
-      targets: '#carousell img',
-      translateX: 600 + 2 * width,
-      loop: true,
-      duration: 40000,
-      easing: 'linear',
-    });
-  };
-
-  const [width, setWidth] = useState(0);
-
-  const handleResize = () => {
-    setWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    if (window) {
-      window.addEventListener('resize', handleResize);
-    }
-    console.log(width);
-  });
-
   return (
-    <div className={style.companies} onClick={animate}>
+    <div className={style.companies}>
       <p>Noen av firmaene vi har kurset:</p>
 
-      <div className={style.carousel__container} onClick={animate}>
-        <div className={style.carousel__wrapper}>
-          <div className={style.carousel__content__wrapper}>
-            <div id="carousell" className={style.carousel__content}>
+      <div className={style.carousel}>
+        <div id="carousell" className={style.slider}>
+          <div className={style.slide__track}>
+            <div className={style.slide}>
               <img src="/images/companies/companies-1.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-2.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-3.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-4.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-4.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-1.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-2.svg" alt="" />
-              <img src="/images/companies/companies-3.svg" alt="" />
-              <img src="/images/companies/companies-4.svg" alt="" />
-              <img src="/images/companies/companies-1.svg" alt="" />
-              <img src="/images/companies/companies-2.svg" alt="" />
+            </div>
+            <div className={style.slide}>
               <img src="/images/companies/companies-3.svg" alt="" />
             </div>
           </div>
