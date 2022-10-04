e5d3bb8149a61ce7f51460d60926b09f0eadb914
TrymVei <tve@variant.no>
2022-10-04 15:44:31 +0200

    fix: changed order on about mobile and added text

--- yhm/src/srcPages/about/ingress/ingress.tsx
+++ yhm/src/srcPages/about/ingress/ingress.tsx
@@ -8,19 +8,26 @@ const Ingress = () => {
           <div>
             <h2>Om oss</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-              feugiat lectus.
+              Young Happy Minds, er en organisasjon som kurser barn og voksne i
+              positiv psykologi, med teknikker som kan hjelpe oss å bli robuste,
+              glade individer som fungerer godt i samspill med andre.
             </p>
             <p>
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
-              egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
-              Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel
-              bibendum lorem. Morbi convallis convallis diam sit amet lacinia.
-              Aliquam in elementum tellus.
+              Young Happy Minds ble stiftet i England i 2017 av Martine Kveim og
+              Yvonne Biggins. YHM har gjennom lokale lag holdt kurs og foredrag
+              i over 15 land, til alt fra barn og unge i fengsler og
+              flyktningeleirer, skoler og SFO til generaler i den Britiske Hær.
+              I Norge drives Young Happy Minds primært av Karina Hollekim (leder
+              digitalt innhold) og Martine Kveim (fagansvarlig).
+            </p>
+            <p>
+              Representanter fra Young Happy Minds har blant annet vært med; å
+              forme "Oxford University Wellbeing Curriculum", vært representert
+              i House of Lords sin arbeidsgruppe om å forme nytt
+              barneskolepensum i England, sittet i designgruppen til
+              Kronprinsparets fond om livsmestrings-prosjektet for ungdom "Flyt"
+              og vært involvert i å utarbeide en rapport for Barne- og
+              likestillingsdepartementet om "Livsmestring i skolen".{' '}
             </p>
           </div>
           <div>
