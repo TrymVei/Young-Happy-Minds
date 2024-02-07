06597cffd539c033bb6430817b6f56da0950e58c
TrymVei <tve@variant.no>
2024-02-07 09:28:35 +0100

    fix: updated broken links for course

--- yhm/src/srcPages/onlineCourse/buy/buy.tsx
+++ yhm/src/srcPages/onlineCourse/buy/buy.tsx
@@ -1,20 +1,20 @@
-import Button from '../../../components/button/button';
-import style from './buy.module.css';
+import Button from "../../../components/button/button";
+import style from "./buy.module.css";
 const Buy = () => {
   return (
     <div id="section" className={style.buy}>
       <p>Pris 5900,-</p>
       <Button
-        color={'white'}
-        bgColor={'#0E645F'}
-        text={'Kjøp kurs'}
+        color={"white"}
+        bgColor={"#0E645F"}
+        text={"Kjøp kurs"}
         big={true}
         img={false}
         href={
-          'https://kurs.younghappyminds.no/purchase/119420-Young-Happy-Minds-et-interaktivt-kurs-i'
+          "https://younghappyminds.simplero.com/purchase/119420-Young-Happy-Minds-et-interaktivt-kurs-i"
         }
-        hoverColor={'#0e645f'}
-        hoverBgColor={'#ffd526'}
+        hoverColor={"#0e645f"}
+        hoverBgColor={"#ffd526"}
       />
     </div>
   );
