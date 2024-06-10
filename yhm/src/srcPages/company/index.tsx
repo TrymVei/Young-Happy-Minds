ba2dc4b7d67d9fb98c042e7c31ff4c396cd2247d
Trym Veiby <110395932+TrymVei@users.noreply.github.com>
2024-06-10 13:55:46 +0200

    feat: remove Equinor company logo from carousel (#32)

--- yhm/src/srcPages/company/index.tsx
+++ yhm/src/srcPages/company/index.tsx
@@ -1,15 +1,11 @@
-import CoursePrice from '../../components/coursePrice/coursePrice';
-import Layout from '../../layout/layout';
-import CourseCompany from '../../utils/courseCompany';
-import AboutCourse from './aboutCourse/aboutCourse';
-import Companies from './companies/companies';
-import Invest from './invest/invest';
-import PercentSection from './percentSection/percentSection';
-import Quote from './quote/quote';
-import Sun from './sun/sun';
+import Layout from "../../layout/layout";
+import AboutCourse from "./aboutCourse/aboutCourse";
+import Companies from "./companies/companies";
+import Invest from "./invest/invest";
+import PercentSection from "./percentSection/percentSection";
+import Sun from "./sun/sun";
 
 const Company = () => {
-  const courses = CourseCompany();
   return (
     <Layout>
       <Invest />
